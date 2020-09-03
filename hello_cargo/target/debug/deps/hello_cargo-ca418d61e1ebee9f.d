7e89a6c7bdb7ee76e1e35cb3e74f8b877ef5332a
Bruce Tan <brucetansh@gmail.com>
2020-09-03 18:56:44 +1200

    hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo-ca418d61e1ebee9f.d
@@ -0,0 +1,5 @@
+/home/bruce/Github/learning-rust/hello_cargo/target/debug/deps/hello_cargo-ca418d61e1ebee9f.rmeta: src/main.rs
+
+/home/bruce/Github/learning-rust/hello_cargo/target/debug/deps/hello_cargo-ca418d61e1ebee9f.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/bruce/Github/learning-rust/hello_cargo/target/debug/hello_cargo: /home/bruce/Github/learning-rust/hello_cargo/src/main.rs
